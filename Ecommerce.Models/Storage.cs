d1860e665f8ceae3c433066d1be262d753d53197
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-07-04 21:42:43 -0300

    Create the Storage entity class

---
+++ Ecommerce.Models/Storage.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models
+{
+    public class Storage
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(50, ErrorMessage = "Name can't be more than 50 characters")]
+        [MinLength(6, ErrorMessage = "Name can't be less than 3 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [MaxLength(150, ErrorMessage = "Description can't be more than 150 characters")]
+        [MinLength(15, ErrorMessage = "Description can't be less than 15 characters")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        public bool Status { get; set; }
+    }
+}
