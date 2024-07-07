756cb380ee8c7457b4d8c681cd7119de71be4370
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-07-07 20:58:48 -0300

    Add the IRepository interface to implement the Repository Pattern

---
+++ Ecommerce.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace Ecommerce.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> Get(int id);
+        Task<IEnumerable<T>> GetAll(
+            Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            string? includeProperties = null,
+            bool istracking = true
+            );
+        Task<T?> GetFirst(
+            Expression<Func<T, bool>>? filter = null,
+            string? includeProperties = null,
+            bool? istracking = true
+            );
+        Task Add(T entity);
+        void Remove(T entity);
+        void BulkRemove(IEnumerable<T> entities);
+    }
+}
