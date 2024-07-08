e8f7a106d01f5702e70360d7630b657b825fd7f5
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-07-08 20:54:18 -0300

    Add UnitofWork Service in Program.cs vfile

--- Ecommerce/Program.cs
+++ Ecommerce/Program.cs
@@ -1,4 +1,6 @@
 using Ecommerce.DataAccess;
+using Ecommerce.DataAccess.Repository;
+using Ecommerce.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +15,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
