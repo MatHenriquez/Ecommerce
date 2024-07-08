4e1b513f09072388863a3e078d768d240c290ad1
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-07-08 20:43:01 -0300

    Add StorageRepository with Update method

---
+++ Ecommerce.DataAccess/Repository/StorageRepository.cs
@@ -0,0 +1,28 @@
+﻿using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Models;
+
+namespace Ecommerce.DataAccess.Repository
+{
+    public class StorageRepository : Repository<Storage>, IStorageRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public StorageRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Storage storage)
+        {
+            var currentStorage = _db.Storages.Find(storage.Id);
+            if (currentStorage != null)
+            {
+                currentStorage.Name = storage.Name;
+                currentStorage.Description = storage.Description;
+                currentStorage.Status = storage.Status;
+
+                _db.SaveChanges();
+            }
+        }
+    }
+}
