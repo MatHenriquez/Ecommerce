cfe6660a711e429c7da8c1a51324f0ebe172bbea
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-07-21 21:15:23 -0300

    Implement the Category repository

---
+++ Ecommerce.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Ecommerce.Models;
+
+namespace Ecommerce.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
