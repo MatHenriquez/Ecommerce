3c89c51049c27471e06d39be65cff4c5d611815a
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-07-21 21:16:03 -0300

    Add Category repository to UnitOfWork

--- Ecommerce.DataAccess/Repository/UnitOfWork.cs
+++ Ecommerce.DataAccess/Repository/UnitOfWork.cs
@@ -11,11 +11,13 @@ namespace Ecommerce.DataAccess.Repository
     {
         private readonly ApplicationDbContext _db;
         public IStorageRepository Storage { get; private set; }
+        public ICategoryRepository Category { get; private set; }
 
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Storage = new StorageRepository(_db);
+            Category = new CategoryRepository(_db);
         }
 
         public void Dispose() => _db.Dispose();
