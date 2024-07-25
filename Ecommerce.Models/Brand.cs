d62afbb1ea9f60d164aff5941bffc788835573d3
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-07-24 22:01:57 -0300

    Create the Brand model

---
+++ Ecommerce.Models/Brand.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models
+{
+    public class Brand
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Brand name is required")]
+        [MaxLength(60, ErrorMessage = "Brand name must be less than 60 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Brand description is required")]
+        [MaxLength(150, ErrorMessage = "Brand description must be less than 150 characters")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Brand status is required")]
+        public bool Status { get; set; }
+    }
+}
